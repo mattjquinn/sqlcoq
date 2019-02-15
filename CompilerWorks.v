cbdaa3e804caba54c9fa238fd88e244e17b066f4
Matt Quinn <matt@mattjquinn.com>
2019-02-14 23:19:32 -0500

    With one where clause.

--- CompilerWorks.v
+++ CompilerWorks.v
@@ -4,22 +4,11 @@ Import ListNotations.
 
 Local Open Scope string_scope.
 
-(*
-Inductive fld : Type :=
-| StrF : string -> fld
-| NatF : nat -> fld.
-
-Inductive row : Type :=
-| Row : (string -> fld) -> row.
-
-Inductive relation : Type :=
-| Rel (rows: list row).
-
-Inductive fromclause : Type :=
-| From (rel: relation).
-*)
 Inductive stmt : Type :=
 | Select (fld: string).
+
+Inductive wclause : Type :=
+| WhereEqual (fld : string) (val : string).
 
 (* Transformer: switches b/w dialects, prove rel is involution. *)
 (* Optimization: remove unused tables from FROM/JOIN clauses. *)
@@ -35,19 +24,37 @@ Proof. reflexivity. Qed.
 
 Definition q1 : stmt := (Select "name").
 
-Fixpoint eval (flds : list string) (rel: list (string->string)) :=
-  map (fun r => (map (fun f => (r f)) flds)) rel.
+Fixpoint evalwhere (w : wclause) (rel : list (string -> string)) :=
+  match w with
+  | WhereEqual wfld wval =>
+    match rel with
+    | hrow::tlrows =>
+      if string_dec (hrow wfld) wval then hrow :: (evalwhere w tlrows)
+                                     else         (evalwhere w tlrows)
+    | _ => []
+    end
+  end.
 
-Example ex2 : (eval ["name"] fruits)
+Fixpoint eval (flds : list string) (rel: list (string->string))
+              (wheres : list wclause) :=
+  match wheres with
+  | whd::wtl => eval flds (evalwhere whd rel) wtl
+  | [] => map (fun r => (map (fun f => (r f)) flds)) rel
+  end.
+
+Example ex2 : (eval ["name"] fruits [])
               = [["orange"] ; ["apple"] ; ["banana"] ; ["kiwi"] ].
 Proof. simpl. reflexivity. Qed.
 
-Example ex3 : (eval ["id";"name"] fruits)
+Example ex3 : (eval ["id";"name"] fruits [])
               = [["1";"orange"] ; ["2";"apple"] ; ["3";"banana"] ; ["4";"kiwi"] ].
 Proof. simpl. reflexivity. Qed.
 
-Example ex4 : (eval ["name";"id"] fruits)
+Example ex4 : (eval ["name";"id"] fruits [])
               = [["orange";"1"] ; ["apple";"2"] ; ["banana";"3"] ; ["kiwi";"4"] ].
 Proof. simpl. reflexivity. Qed.
 
+Example ex5 : (eval ["id";"name"] fruits [(WhereEqual "name" "kiwi")])
+              = [ ["4";"kiwi"] ].
+Proof. simpl. reflexivity. Qed.
 
