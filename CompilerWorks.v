212d503bba1b1af6529c4209a4c018df739e05be
Matt Quinn <matt@mattjquinn.com>
2019-02-18 14:36:31 -0800

    Left join added.

--- CompilerWorks.v
+++ CompilerWorks.v
@@ -7,16 +7,18 @@ Import ListNotations.
 
 Local Open Scope string_scope.
 
+Definition relation : Type := (list string) * list (string -> string).
+
 Inductive jclause : Type :=
-| Join (relation: list (string -> string)) (onlhs : string) (onrhs : string).
+| LeftJoin (rel : relation) (onlhs : string) (onrhs : string).
 
 Inductive wclause : Type :=
 | WhereEqual (fld : string) (val : string).
 
-Definition relation : Type := (list string) * list (string -> string).
-
 Inductive sqlstmt : Type :=
-| Select (flds: list string) (rel: relation) (wheres: option (list wclause)).
+| Select (flds: list string) (rel: relation)
+         (join: option jclause)
+         (wheres: option (list wclause)).
 
 Definition fruits : relation :=
   (["id" ; "name" ; "price"] ,
@@ -55,31 +57,49 @@ Fixpoint evalwhere (w : wclause) (rel : list (string -> string)) :=
   end.
  *)
 
-Fixpoint evalstmt (stmt : sqlstmt) : relation :=
+ (* Old wheres: | whd::wtl => eval flds (evalwhere whd rel) wtl *)
+Definition evalstmt (stmt : sqlstmt) : relation :=
+  let fix select_fields (flds : list string) (row : (string -> string))
+                        : (string -> string) :=
+    match flds with
+    | f::fs => (fun c => if (string_dec c f)
+                        then (row c) else ((select_fields fs) row) c)
+    | [] => (fun _ => "")
+    end
+  in 
   match stmt with
-  | Select fields rel _ =>
-    let fix select_fields (flds : list string) (row : (string -> string))
-                          : (string -> string) :=
-      match flds with
-      | f::fs => (fun c => if (string_dec c f)
-                          then (row c) else ((select_fields fs) row) c)
-      | [] => (fun _ => "")
-      end
-    in 
-    (fields, (map (select_fields fields) (snd rel)))
-    (* Old wheres: | whd::wtl => eval flds (evalwhere whd rel) wtl *)
+  | Select fields rel None _  => (fields, (map (select_fields fields) (snd rel)))
+  | Select fields rel (Some (LeftJoin reljoin fld_l fld_r)) _  =>
+    let fix ljoin (rows_r : list (string -> string)) (row_l : (string -> string))
+                  : list (string -> string) :=
+        match rows_r with
+        | row_r::rs =>
+            if string_dec (row_l fld_l) (row_r fld_r) then
+              (fun c => let a := (row_l c) in
+                        if (string_dec a "") then (row_r c) else a)
+                :: (ljoin rs row_l)
+            else (ljoin rs row_l)
+        | [] => []
+        end
+    in
+    (* This is a left join; if right relation has no records, return left as is. *)
+    match (snd rel) with
+    | _::_ =>  (fields, (map (select_fields fields)
+                 (flat_map (fun row_l => ljoin (snd reljoin) (row_l)) (snd rel))))
+    | [] => (fields, (map (select_fields fields) (snd rel)))
+    end
   end.
 
-Example ex2 : (render (evalstmt (Select ["name"] fruits None)))
+Example ex2 : (render (evalstmt (Select ["name"] fruits None None)))
               = [["name"] ; ["orange"] ; ["apple"] ; ["banana"] ; ["kiwi"] ].
 Proof. unfold render. simpl. reflexivity. Qed. 
 
-Example ex3 : (render (evalstmt (Select ["id";"name"] fruits None)))
+Example ex3 : (render (evalstmt (Select ["id";"name"] fruits None None)))
               = [["id";"name"] ; ["1";"orange"] ; ["2";"apple"] ;
                    ["3";"banana"] ; ["4";"kiwi"] ].
 Proof. simpl. reflexivity. Qed.
 
-Example ex4 : (render (evalstmt (Select ["name" ; "id"] fruits None)))
+Example ex4 : (render (evalstmt (Select ["name" ; "id"] fruits None None)))
               = [["name";"id"] ; ["orange";"1"] ; ["apple";"2"] ; ["banana";"3"] ; ["kiwi";"4"] ].
 Proof. unfold render. simpl. reflexivity. Qed.
 
@@ -99,14 +119,16 @@ Example ex7 : (eval ["name"] fruits [(WhereEqual "price" "5");(WhereEqual "id" "
 Proof. simpl. reflexivity. Qed.
  *)
 
-Example ex6_subquery : (render (evalstmt (Select ["name"]
-                               (evalstmt (Select ["price";"name"] fruits None)) None)))
+Example ex6_subquery :
+  (render (evalstmt (Select ["name"]
+           (evalstmt (Select ["price";"name"] fruits None None)) None None)))
               = [["name"] ; ["orange"] ; ["apple"] ; ["banana"] ; ["kiwi"] ].
 Proof. unfold render. simpl. reflexivity. Qed.
 
-Definition stores : list (string -> string) := [
-    fun c => match c with "sid"=>"1"|"sname"=>"La Tienda"|"sfruit"=>"4"|_=>"" end ;
-    fun c => match c with "sid"=>"2"|"sname"=>"Duka 88"|"sfruit"=>"1"|_=>"" end].
+Definition stores : relation :=
+  (["sid";"sname";"sfruit"] ,
+  [fun c => match c with "sid"=>"1"|"sname"=>"La Tienda"|"sfruit"=>"4"|_=>"" end ;
+   fun c => match c with "sid"=>"2"|"sname"=>"Duka 88"|"sfruit"=>"1"|_=>"" end]).
 
 (* TODO: Define a join evaluator that can combine two relations. I envision
    this is a destructuring of each (string -> string) such that the join
@@ -115,14 +137,10 @@ Definition stores : list (string -> string) := [
    is that the evaluator shouldn't return lists of strings, it should return
    proper relations. A separate function (i.e., "render") should "evaluate" the
    relation itself into actual lists so that we can write proofs about it.
-*)
-Fixpoint eval2 (flds : list string) (rel: list (string->string))
-               (join : jclause) (wheres : list wclause) :=
-  match wheres with
-  | whd::wtl => eval flds (evalwhere whd rel) wtl
-  | [] => map (fun r => (map (fun f => (r f)) flds)) rel
-  end.
+ *)
 
-Example exj1 : (eval2 ["sname" ; "name"] stores (Join fruits "sfruit" "id") [])
-               = [["orange"] ; ["apple"] ; ["banana"] ; ["kiwi"] ].
-Proof. simpl. reflexivity. Qed.
+
+Example ex7_join1 : (render (evalstmt (Select ["sname" ; "name"] stores
+                                        (Some (LeftJoin fruits "sfruit" "id")) None)))
+              = [["sname";"name"];["La Tienda";"kiwi"];["Duka 88";"orange"]].
+Proof. unfold render. simpl. reflexivity. Qed.
